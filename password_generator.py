fe586a7a857e6b4e4a4c39b2844f7d0328958e0f
henwh0 <hmcginnis96@gmail.com>
2026-05-20 23:29:58 -0400

    Update password_generator.py
    Now using secrets module instead of random

--- password_generator.py
+++ password_generator.py
@@ -1,29 +1,41 @@
-import random
+import secrets
 import string
 
-class Tcolors:
+class Colors:
     CYAN = '\033[0;36m'
     GREEN = '\033[0;32m'
     NC = '\033[0m'
 
-def generate_password():
+def cprint(msg, Color=Colors.NC):
+    print(f"{Color}{msg}{Colors.NC}")
 
-    password_length = random.randint(10, 20)
 
-    num_letters = random.randint(4, password_length - 4)
-    remaining = password_length - num_letters
-    num_numbers = random.randint(1, min(4, remaining - 1))
-    num_symbols = remaining - num_numbers  # The rest are symbols
+def generate_pw():
 
-    letters = random.choices(string.ascii_letters, k=num_letters)
-    numbers = random.choices(string.digits, k=num_numbers)
-    symbols = random.choices("!@#$%^&*()-_=+", k=num_symbols)
+    pw_length = secrets.choice(range(10, 21))
+    num_letters = secrets.randbelow(pw_length - 4) + 4
+    remaining = pw_length - num_letters
+    num_numbers = secrets.randbelow(remaining + 1)
+    num_symbols = remaining - num_numbers
+
+    letters = [
+        secrets.choice(string.ascii_letters)
+        for _ in range(num_letters)
+    ]
+
+    numbers = [
+        secrets.choice(string.digits)
+        for _ in range(num_numbers)
+    ]
+
+    symbols = [
+        secrets.choice("!@#$%^&*-_=+")
+        for _ in range(num_symbols)
+    ]
 
     password_list = letters + numbers + symbols
-    random.shuffle(password_list)
-    password = "".join(password_list)
-    return password
+    secrets.SystemRandom().shuffle(password_list)
+    return "".join(password_list)
 
-
-print(f"\n{Tcolors.CYAN}=== Password_Generator ==={Tcolors.NC}")
-print(f"Your secure password is: {Tcolors.GREEN}{generate_password()}{Tcolors.NC}\n")
+cprint(f"\n=== Password_Generator ===", Colors.CYAN)
+cprint(f"Your secure password is: {generate_pw()}\n", Colors.GREEN)
